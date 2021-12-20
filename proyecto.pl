c7aace99a1de7dfc4466adc6eeae793d25b88c72
Nicovenegas <nicovenegas18@gmail.com>
2021-12-20 16:24:00 -0300

    correccion de paradigmadocs

--- proyecto.pl
+++ proyecto.pl
@@ -2,6 +2,8 @@ date(Dia, Mes, Agno, [Dia, Mes, Agno]):- integer(Dia), integer(Mes),
 integer(Agno).
 
 
-paradigmadocs(Nombre, Date, [Nombre, Date]) :-
+paradigmadocs(Nombre, [Dia, Mes, Agno], [Nombre, Date]) :-
     string(Nombre),
-    date(Date).
+    date(Dia, Mes, Agno,Date).
+
+
