676fb410bdb163f57fd01a38758f57ee7f098635
Nicolas <nicolas.venegas.a@usach.cl>
2021-12-31 15:38:38 -0300

    Update proyecto.pl

--- proyecto.pl
+++ proyecto.pl
@@ -12,7 +12,7 @@ emptyList([]).
 %   Significado salida paradimadocs =[nombre, Fecha creacion, Lista de usuarios, lista de Documentos, usuario activo]
 paradigmadocs(Nombre, Date, [Nombre, Date, [], [], []]) :-
     string(Nombre),
-    date(Dia, Mes, Agno,Date).
+    date(_, _, _,Date).
 
 
 
@@ -25,22 +25,22 @@ paraIsLogin(Sn1) :-
 usuario(Nombre, Password,Date, [Nombre, Password, Date]) :-
     string(Nombre),
     string(Password),
-    date(Dia, Mes, Agno, Date).
+    date(_, _, _, Date).
 
 %                                                           [id, nombre, fecha, autor, contenido, lista de permisos, lista de usuarios compartidos]
-documento(Id, Nombre, [Dia, Mes, Agno], Autor, Contenido, [Id, Nombre, Date, Autor, Contenido, [], []]) :-
+documento(Id, Nombre, Date, Autor, Contenido, [Id, Nombre, Date, Autor, Contenido, [], []]) :-
     integer(Id),
     string(Nombre),
-    date(Dia, Mes, Agno, Date),
+    date(_, _, _, Date),
     string(Autor),
     string(Contenido).
 
 documentGetter([Id, Nombre, Date, Autor, Contenido, Permisos, UsuariosCompartidos], [Id, Nombre, Date, Autor, Contenido, Permisos, UsuariosCompartidos]).
 
 addDocument(Sn1, Document, SOut) :-
-    paraGetter(Sn1, [N, F, LU, LD, UA]),
+    paraGetter(Sn1, [N, F, _, LD, _]),
     append(LD, [Document], LD1),
-    paraGetter([N, F, LU, LD1, UA], SOut).
+    paraGetter([_, _, _, LD1, _], SOut).
 
 
 getDocumentById([Document|_], Id, DocumentOut) :-
@@ -51,12 +51,13 @@ getDocumentById([_|Resto], Id, DocumentOut) :-
 
 create(Sn1, Fecha, Nombre, Contenido, SOut) :-
     ((paraIsLogin(Sn1),
-    paraGetter(Sn1, [_, _, _, LD, UA]),
-    date(Dia, Mes, Agno, Fecha),
+    paraGetter(Sn1, [N, F, A, LD, UA]),
+    date(_, _, _, Fecha),
     usuario(NombreA, _, _, UA),
     length(LD, Id),
     documento(Id, Nombre, Fecha, NombreA, Contenido, Document),
-    addDocument(Sn1, Document, SOut))
+    addDocument(Sn1, Document, SOut),
+    paraGetter(SOut, [N, F, A, _, []]) )
     ; Sn1 = SOut).
 
 
@@ -67,8 +68,8 @@ create(Sn1, Fecha, Nombre, Contenido, SOut) :-
 
 
 
-canLogin(Nombre, Password, [[Nombre, Password,_]|Resto]).
-canLogin(Nombre, Password, [[N, P,_]|Resto]) :-
+canLogin(Nombre, Password, [[Nombre, Password,_]|_]).
+canLogin(Nombre, Password, [[_, _,_]|Resto]) :-
     string(Nombre),
     string(Password),
     canLogin(Nombre, Password, Resto).
@@ -76,8 +77,8 @@ canLogin(Nombre, Password, [[N, P,_]|Resto]) :-
 
 
 
-canRegister(Nombre, [[Nombre, _,_]|Resto]).
-canRegister(Nombre, [[N, _,_]|Resto]) :-
+canRegister(Nombre, [[Nombre, _,_]|_]).
+canRegister(Nombre, [[_, _,_]|Resto]) :-
     string(Nombre),
     canRegister(Nombre, Resto).
 
@@ -90,12 +91,12 @@ register(Nombre, Password, Date, [N, F, LU, LD, UA], [N, F, LUA, LD, UA]):-
 
 
 %ARREGLAR, solo parametros Sn1 y SOut
-login( Nombre, Password, [N, F, LU, LD UA], [N, F, LU,LD NUA]):-
+login( Nombre, Password, [N, F, LU, LD, UA], [N, F, LU,LD, NUA]):-
     (
         (
-            emptyList(UA),
-            not(canLogin(Nombre, Password, LU)),
-            append(LU, [Nombre, Password], NUA)
+            not(paraIsLogin([N, F, LU, LD, UA])),
+            canLogin(Nombre, Password, LU),
+            append(UA, [Nombre, Password, [-1,-1,-1]], NUA)
         );
         NUA = UA
     ).
@@ -112,10 +113,15 @@ login( Nombre, Password, [N, F, LU, LD UA], [N, F, LU,LD NUA]):-
 
 /*
 
-paradigmadocs("Word", [27, 12, 2021], Word), register("nico", "1234", Word, Word1), register("nico", "123424234", Word1, Word2).
+paradigmadocs("Word", [27, 12, 2021], Word), register("nico", "1234", [03,05,2020], Word, Word1), register("nico", "123424234", Word1, Word2).
+
+paradigmadocs("Word", [27, 12, 2021], Word), register("nico", "1234", [03,05,2020], Word, Word1),              
+login("nico", "1234", Word1, Word2).
 
 
-
+paradigmadocs("Word", [27, 12, 2021], Word), register("nico", "1234", [03,05,2020], Word, Word1),   
+login("nico", "1234", Word1, Word2),
+create(Word2, [4,4,2021], "Primer Documento", "Contenido 1", Word3).
 
 */
 
